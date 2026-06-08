f4ee4b3b6e756228bf0a6eeaaf5627ef0cb1983c
Apur0y <apuroy2785@gmail.com>
2026-06-08 22:22:46 +0600

    add new frontend

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(cookieParser());
 
 // Configure CORS based on environment
-const allowedOrigins = (process.env.FRONTEND_URL || "http://localhost:5173").split(",");
+const allowedOrigins = (process.env.ALLOWED_ORIGINS || "http://localhost:5173").split(",");
 
 app.use(
   cors({
